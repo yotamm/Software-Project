58b119759e77463bd58b0c47fa9fcfa4295ffa3b
yotam ronen <yotronen@gmail.com>
2016-05-22 13:34:57 +0300

    SPBPriorityQueue.c was missing. added it with all the functions from the header file

---
+++ Assignment3/SPBPriorityQueue.c
@@ -0,0 +1,78 @@
+#include "SPBPriorityQueue.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+struct sp_bp_queue_t{
+
+};
+
+//TODO
+SPBPQueue spBPQueueCreate(int maxSize){
+
+}
+
+//TODO
+SPBPQueue spBPQueueCopy(SPBPQueue source){
+
+}
+
+//TODO
+void spBPQueueDestroy(SPBPQueue source){
+
+}
+
+//TODO
+void spBPQueueClear(SPBPQueue source){
+
+}
+
+//TODO
+int spBPQueueSize(SPBPQueue source){
+
+}
+
+//TODO
+int spBPQueueGetMaxSize(SPBPQueue source){
+
+}
+
+//TODO
+SP_BPQUEUE_MSG spBPQueueEnqueue(SPBPQueue source, SPListElement element){
+
+}
+
+//TODO
+SP_BPQUEUE_MSG spBPQueueDequeue(SPBPQueue source){
+
+}
+
+//TODO
+SPListElement spBPQueuePeek(SPBPQueue source){
+
+}
+
+//TODO
+SPListElement spBPQueuePeekLast(SPBPQueue source){
+
+}
+
+//TODO
+double spBPQueueMinValue(SPBPQueue source){
+
+}
+
+//TODO
+double spBPQueueMaxValue(SPBPQueue source){
+
+}
+
+//TODO
+bool spBPQueueIsEmpty(SPBPQueue source){
+
+}
+
+//TODO
+bool spBPQueueIsFull(SPBPQueue source){
+
+}
