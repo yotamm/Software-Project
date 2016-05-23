84be7d9cca7c0b0ae29379964421d8b5c7da8401
dror <drorbrun.man@gmail.com>
2016-05-23 14:52:18 +0300

    added documentation for queue.c and queue.h
    also went threw the functions trying to fix/sort some stuff
    
    some still doesnt work.. but seems better

--- Assignment3/SPBPriorityQueue.c
+++ Assignment3/SPBPriorityQueue.c
@@ -4,70 +4,116 @@
 #include <stdlib.h>
 #include <assert.h>
 
-struct sp_bp_queue_t{
+struct sp_bp_queue_t {
 	int maxSize;
 	int currentSize;
 	SPList boundedPriorityQueue;
 };
 
-SPBPQueue spBPQueueCreate(int maxSize){
-	struct sp_bp_queue_t *queue = malloc(sizeof(struct sp_bp_queue_t));
-	queue.currentSize=0;
-	queue.maxSize=maxSize;
-	queue.boundedPriorityQueue=spListCreate();
+/*
+ * creates an empty queue with a given maximum capacity
+ *
+ * @param maxSize is maximum capacity
+ * @return new empty queue with a given maximum capacity
+ */
+SPBPQueue spBPQueueCreate(int maxSize) {
+	SPBPQueue queue = (SPBPQueue) malloc(sizeof(struct sp_bp_queue_t));
+	queue->currentSize = 0;
+	queue->maxSize = maxSize;
+	queue->boundedPriorityQueue = spListCreate();
 	return queue;
 	//TODO deal with errors
 }
 
-SPBPQueue spBPQueueCopy(SPBPQueue source){
-	struct sp_bp_queue_t *queue = malloc(sizeof(struct sp_bp_queue_t));
-	queue.currentSize=source->currentSize;
-	queue.maxSize=source->maxSize;
-	queue.boundedPriorityQueue=spListCopy(source->boundedPriorityQueue);
+/*
+ * creates a copy of a given queue
+ *
+ * @param source is given queue
+ * @return SPBPQueue which is a copy of source
+ */
+SPBPQueue spBPQueueCopy(SPBPQueue source) {
+	SPBPQueue queue = (SPBPQueue) malloc(sizeof(struct sp_bp_queue_t));
+	queue->currentSize = source->currentSize;
+	queue->maxSize = source->maxSize;
+	queue->boundedPriorityQueue = spListCopy(source->boundedPriorityQueue);
 	return queue;
 	//TODO deal with errors
 }
 
-void spBPQueueDestroy(SPBPQueue source){
+/*
+ * frees all memory allocation associated with the queue
+ *
+ * @param source is the queue to free memmory allocs from
+ * @return
+ */
+void spBPQueueDestroy(SPBPQueue source) {
 	spListDestroy(source->boundedPriorityQueue);
 	free(source);
 }
 
-void spBPQueueClear(SPBPQueue source){
-	enum SP_LIST_MSG MSG;
-	MSG = spListClear(source->boundedPriorityQueue);
-	source->currentSize=0;
-	//TODO deal with errors
+/*
+ * removes all the elements in the queue
+ *
+ * @param soure is the queue to remove all elems from
+ * @return
+ */
+void spBPQueueClear(SPBPQueue source) {
+	if(source == NULL)
+		return;
+	source->currentSize = 0; //set size to be 0 since no elems in queue
+	if(!(source->boundedPriorityQueue == NULL))
+		//clear queue if it's not null
+		spListClear(source->boundedPriorityQueue);
 }
 
-int spBPQueueSize(SPBPQueue source){
+/*
+ * returns the number of elements in the queue
+ *
+ * @param queue whom we count elems from
+ * @return number of elements in the queue
+ */
+int spBPQueueSize(SPBPQueue source) {
 	return source->currentSize;
-	//TODO deal with errors
 }
 
-int spBPQueueGetMaxSize(SPBPQueue source){
+/*
+ * returns the maximum capacity of the queue
+ *
+ * @param source whom we want his max cap'
+ * @return source's max cap'
+ */
+int spBPQueueGetMaxSize(SPBPQueue source) {
 	return source->maxSize;
-	//TODO deal with errors
 }
 
-SP_BPQUEUE_MSG spBPQueueEnqueue(SPBPQueue source, SPListElement element){
-	if (spBPQueueIsEmpty(source)){
-		spListInsertFirst(source->boundedPriorityQueue);
+/*
+ * Inserts a new copy (which is allocated) element to the queue
+ *
+ * @param source - the queue we enter the new elemnt into
+ * @param element - the element we enter to source
+ * @return msg containing info on the insert
+ */
+SP_BPQUEUE_MSG spBPQueueEnqueue(SPBPQueue source, SPListElement element) {
+	int j = 0, i = 0;
+	if (spBPQueueIsEmpty(source)) {
+		spListInsertFirst(source->boundedPriorityQueue, element);
 		//TODO return proper message enum
 	}
-	if (spBPQueueIsFull(source)){// remove the highest value in the list because it is full
+	if (spBPQueueIsFull(source)) { // remove the highest value in the list because it is full
+		//TODO isn't the last node got highest value? we have spListGetLast or so?
 		spListGetFirst(source->boundedPriorityQueue);
-		for (int j=0; j<spListGetSize(source->boundedPriorityQueue); j++){
+		for (; j < spListGetSize(source->boundedPriorityQueue); j++) {
 			spListGetNext(source->boundedPriorityQueue);
 		}
 		spListRemoveCurrent(source->boundedPriorityQueue);
 	}
 
 	SPListElement toCompare = spListGetFirst(source->boundedPriorityQueue);
-	enum SP_LIST_MSG MSG;
-	for (int i=0; i<source->currentSize; i++){
-		if (spListElementCompare(element, toCompare) == -1){
-			MSG = spListInsertBeforeCurrent(source->boundedPriorityQueue, element);
+	SP_LIST_MSG MSG;
+	for (; i < source->currentSize; i++) {
+		if (spListElementCompare(element, toCompare) == -1) {
+			MSG = spListInsertBeforeCurrent(source->boundedPriorityQueue,
+					element);
 			//TODO return proper message enum
 			source->currentSize = spListGetSize(source);
 		}
@@ -78,39 +124,79 @@ SP_BPQUEUE_MSG spBPQueueEnqueue(SPBPQueue source, SPListElement element){
 	source->currentSize = spListGetSize(source);
 }
 
-SP_BPQUEUE_MSG spBPQueueDequeue(SPBPQueue source){
-	enum SP_LIST_MSG MSG;
+/*
+ * removes the element with the lowest value
+ *
+ * @param source - the queue we remove the element with the lowest value
+ * @return msg with info on the remove proccess
+ */
+SP_BPQUEUE_MSG spBPQueueDequeue(SPBPQueue source) {
 	spListGetFirst(source->boundedPriorityQueue);
-	MSG = spListRemoveCurrent(source->boundedPriorityQueue);
-	//TODO return proper message enum
+	//remove first node which has lowest value, and return proper msg
+	return (spListRemoveCurrent(source->boundedPriorityQueue));
 }
 
-SPListElement spBPQueuePeek(SPBPQueue source){
-	return spListElementCopy(spListGetFirst(source));
-	//TODO deal with errors
+/*
+ * returns a new copy of the element with the lowest value
+ *
+ * @param source - queue we get element with the lowest value from
+ * @return a new copy of the element with the lowest value
+ */
+SPListElement spBPQueuePeek(SPBPQueue source) {
+	SPListElement temp_first = spListGetFirst(source->boundedPriorityQueue);
+	return spListElementCopy(temp_first);
 }
 
-SPListElement spBPQueuePeekLast(SPBPQueue source){
+/*
+ * returns a new copy of the element with the highest value
+ *
+ * @param source - queue we get element with the highest value from
+ * @return a new copy of the element with the highest value
+ */
+SPListElement spBPQueuePeekLast(SPBPQueue source) {
 	return spListElementCopy(source->boundedPriorityQueue->tail->data);
-	//TODO deal with errors
 }
 
-double spBPQueueMinValue(SPBPQueue source){
-	return spListGetFirst(source)->value;
-	//TODO deal with errors
+/*
+ * returns the minimum value in the queue
+ *
+ * @param source the queue we get value from
+ * @return the min value in the queue
+ */
+double spBPQueueMinValue(SPBPQueue source) {
+	//min value in the first node in queue
+	return (spListElementGetValue(spListGetFirst(source->boundedPriorityQueue)));
 }
 
-double spBPQueueMaxValue(SPBPQueue source){
-	return source->boundedPriorityQueue->tail->data->value;
-	//TODO deal with errors
+/*
+ * returns the maximum value in the queue
+ *
+ * @param source the queue we get value from
+ * @return the max value in the queue
+ */
+double spBPQueueMaxValue(SPBPQueue source) {
+	//max value in the last node in queue
+	return (source->boundedPriorityQueue->tail->data->value);
 }
 
-bool spBPQueueIsEmpty(SPBPQueue source){
-	return (source->currentSize) == 0;
-	//TODO deal with errors
+/*
+ * checks if queue is empty
+ *
+ * @param queue to check
+ * @return true if the queue is empty, false otherwise
+ */
+bool spBPQueueIsEmpty(SPBPQueue source) {
+	//queue is empty <--> (source->currentSize) == 0
+	return ((source->currentSize) == 0);
 }
 
-bool spBPQueueIsFull(SPBPQueue source){
-	return (source->currentSize) == (source->maxSize);
-	//TODO deal with errors
+/*
+ * checks if queue is full
+ *
+ * @param queue to check
+ * @return true if the queue is full, false otherwise
+ */
+bool spBPQueueIsFull(SPBPQueue source) {
+	//queue is full <--> (source->currentSize) == (source->maxSize)
+	return ((source->currentSize) == (source->maxSize));
 }
--- Assignment3/SPBPriorityQueue.h
+++ Assignment3/SPBPriorityQueue.h
@@ -21,73 +21,116 @@ typedef enum sp_bp_queue_msg_t {
 	SP_BPQUEUE_SUCCESS
 } SP_BPQUEUE_MSG;
 
-/**
- * TODO Complete documentation
+/*
+ * creates an empty queue with a given maximum capacity
+ *
+ * @param maxSize is maximum capacity
+ * @return new empty queue with a given maximum capacity
  */
 SPBPQueue spBPQueueCreate(int maxSize);
 
-/**
- * TODO Complete documentation
+/*
+ * creates a copy of a given queue
+ *
+ * @param source is given queue
+ * @return SPBPQueue which is a copy of source
  */
 SPBPQueue spBPQueueCopy(SPBPQueue source);
 
-/**
- * TODO Complete documentation
+/*
+ * frees all memory allocation associated with the queue
+ *
+ * @param source is the queue to free memmory allocs from
+ * @return
  */
 void spBPQueueDestroy(SPBPQueue source);
 
-/**
- * TODO Complete documentation
+/*
+ * removes all the elements in the queue
+ *
+ * @param soure is the queue to remove all elems from
+ * @return
  */
 void spBPQueueClear(SPBPQueue source);
 
-/**
- * TODO Complete documentation
+/*
+ * returns the number of elements in the queue
+ *
+ * @param queue whom we count elems from
+ * @return number of elements in the queue
  */
 int spBPQueueSize(SPBPQueue source);
 
-/**
- * TODO Complete documentation
+/*
+ * returns the maximum capacity of the queue
+ *
+ * @param source whom we want his max cap'
+ * @return source's max cap'
  */
 int spBPQueueGetMaxSize(SPBPQueue source);
 
-/**
- * TODO Complete documentation
+/*
+ * Inserts a new copy (which is allocated) element to the queue
+ *
+ * @param source - the queue we enter the new elemnt into
+ * @param element - the element we enter to source
+ * @return msg containing info on the insert
  */
 SP_BPQUEUE_MSG spBPQueueEnqueue(SPBPQueue source, SPListElement element);
 
-/**
- * TODO Complete documentation
+/*
+ * removes the element with the lowest value
+ *
+ * @param source - the queue we remove the element with the lowest value
+ * @return msg with info on
  */
 SP_BPQUEUE_MSG spBPQueueDequeue(SPBPQueue source);
 
-/**
- * TODO Complete documentation
+/*
+ * returns a new copy of the element with the lowest value
+ *
+ * @param source - queue we get element with the lowest value from
+ * @return a new copy of the element with the lowest value
  */
 SPListElement spBPQueuePeek(SPBPQueue source);
 
-/**
- * TODO Complete documentation
+/*
+ * returns a new copy of the element with the highest value
+ *
+ * @param source - queue we get element with the highest value from
+ * @return a new copy of the element with the highest value
  */
 SPListElement spBPQueuePeekLast(SPBPQueue source);
 
-/**
- * TODO Complete documentation
+/*
+ * returns the minimum value in the queue
+ *
+ * @param source the queue we get value from
+ * @return the min value in the queue
  */
 double spBPQueueMinValue(SPBPQueue source);
 
-/**
- * TODO Complete documentation
+/*
+ * returns the maximum value in the queue
+ *
+ * @param source the queue we get value from
+ * @return the max value in the queue
  */
 double spBPQueueMaxValue(SPBPQueue source);
 
-/**
- * TODO Complete documentation
+/*
+ * checks if queue is empty
+ *
+ * @param queue to check
+ * @return true if the queue is empty, false otherwise
  */
 bool spBPQueueIsEmpty(SPBPQueue source);
 
-/**
- * TODO Complete documentation
+/*
+ * checks if queue is full
+ *
+ * @param queue to check
+ * @return true if the queue is full, false otherwise
  */
 bool spBPQueueIsFull(SPBPQueue source);
 
