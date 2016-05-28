0cdd7575d11279dfd58097fcb80ef941f3263645
yotam ronen <yotronen@gmail.com>
2016-05-28 23:21:39 +0300

    error testing and messages

--- Assignment3/SPBPriorityQueue.c
+++ Assignment3/SPBPriorityQueue.c
@@ -16,11 +16,17 @@ struct sp_bp_queue_t {
  * @return new empty queue with a given maximum capacity
  */
 SPBPQueue spBPQueueCreate(int maxSize) {
+	if (maxSize<1){
+		return NULL;
+	}
 	SPBPQueue queue = (SPBPQueue) malloc(sizeof(struct sp_bp_queue_t));
+	if (queue == NULL){
+		printf("An error occurred - allocation failure\n");
+		return NULL;
+	}
 	queue->maxSize = maxSize;
 	queue->boundedPriorityQueue = spListCreate();
 	return queue;
-	//TODO deal with errors
 }
 
 /*
@@ -30,11 +36,17 @@ SPBPQueue spBPQueueCreate(int maxSize) {
  * @return SPBPQueue which is a copy of source
  */
 SPBPQueue spBPQueueCopy(SPBPQueue source) {
+	if (source == NULL){
+		return NULL;
+	}
 	SPBPQueue queue = (SPBPQueue) malloc(sizeof(struct sp_bp_queue_t));
+	if (queue == NULL){
+		printf("An error occurred - allocation failure\n");
+		return NULL;
+	}
 	queue->maxSize = source->maxSize;
 	queue->boundedPriorityQueue = spListCopy(source->boundedPriorityQueue);
 	return queue;
-	//TODO deal with errors
 }
 
 /*
@@ -91,30 +103,44 @@ int spBPQueueGetMaxSize(SPBPQueue source) {
  */
 SP_BPQUEUE_MSG spBPQueueEnqueue(SPBPQueue source, SPListElement element) {
 	int j = 0, i = 0;
+	SP_BPQUEUE_MSG msg;
+	SP_LIST_MSG listMsg;
+	if (source == NULL || element == NULL){
+		msg = SP_BPQUEUE_INVALID_ARGUMENT;
+		return msg;
+	}
 	if (spBPQueueIsEmpty(source)) {
-		spListInsertFirst(source->boundedPriorityQueue, element);
-		//TODO return proper message enum
+		listMsg = spListInsertFirst(source->boundedPriorityQueue, element);
+		msg=switchListMessage(listMsg);
+		return msg;
 	}
 	if (spBPQueueIsFull(source)) { // remove the highest value in the list because it is full
 		spListGetFirst(source->boundedPriorityQueue);
 		for (; j < spListGetSize(source->boundedPriorityQueue); j++) {
 			spListGetNext(source->boundedPriorityQueue);
 		}
-		spListRemoveCurrent(source->boundedPriorityQueue);
+		listMsg = spListRemoveCurrent(source->boundedPriorityQueue);
+		msg=switchListMessage(listMsg);
+		if(msg != SP_BPQUEUE_SUCCESS){
+			return msg;
+		}
 	}
 
 	SPListElement toCompare = spListGetFirst(source->boundedPriorityQueue);
-	SP_LIST_MSG MSG;
 	for (; i < spBPQueueSize(source); i++) {
 		if (spListElementCompare(element, toCompare) == -1) {
-			MSG = spListInsertBeforeCurrent(source->boundedPriorityQueue,
+			listMsg = spListInsertBeforeCurrent(source->boundedPriorityQueue,
 					element);
-			//TODO return proper message enum
+			msg=switchListMessage(listMsg);
+			if(msg != SP_BPQUEUE_SUCCESS){
+				return msg;
+			}
 		}
 		toCompare = spListGetNext(source->boundedPriorityQueue);
 	}
-	MSG = spListInsertLast(source->boundedPriorityQueue, element);
-	//TODO return proper message enum
+	listMsg = spListInsertLast(source->boundedPriorityQueue, element);
+	msg=switchListMessage(listMsg);
+	return msg;
 }
 
 /*
@@ -124,9 +150,16 @@ SP_BPQUEUE_MSG spBPQueueEnqueue(SPBPQueue source, SPListElement element) {
  * @return msg with info on the remove proccess
  */
 SP_BPQUEUE_MSG spBPQueueDequeue(SPBPQueue source) {
+	SP_LIST_MSG listMsg;
+	SP_BPQUEUE_MSG msg;
+	if (source == NULL){
+		return SP_BPQUEUE_INVALID_ARGUMENT;
+	}
 	spListGetFirst(source->boundedPriorityQueue);
 	//remove first node which has lowest value, and return proper msg
-	return (spListRemoveCurrent(source->boundedPriorityQueue));
+	listMsg = spListRemoveCurrent(source->boundedPriorityQueue);
+	msg=switchListMessage(listMsg);
+	return msg;
 }
 
 /*
@@ -136,6 +169,9 @@ SP_BPQUEUE_MSG spBPQueueDequeue(SPBPQueue source) {
  * @return a new copy of the element with the lowest value
  */
 SPListElement spBPQueuePeek(SPBPQueue source) {
+	if (source == NULL){
+		return NULL;
+	}
 	SPListElement temp_first = spListGetFirst(source->boundedPriorityQueue);
 	return spListElementCopy(temp_first);
 }
@@ -147,11 +183,16 @@ SPListElement spBPQueuePeek(SPBPQueue source) {
  * @return a new copy of the element with the highest value
  */
 SPListElement spBPQueuePeekLast(SPBPQueue source) {
+	if (source == NULL){
+		return NULL;
+	}
+	SPListElement toReturn;
 	spListGetFirst(source->boundedPriorityQueue);
 	for (int j=0 ; j < spListGetSize(source->boundedPriorityQueue); j++) {
 		spListGetNext(source->boundedPriorityQueue);
 	}
-	return spListElementCopy(spListGetCurrent(source->boundedPriorityQueue));
+	toReturn = spListElementCopy(spListGetCurrent(source->boundedPriorityQueue));
+	return toReturn;
 }
 
 /*
@@ -161,8 +202,12 @@ SPListElement spBPQueuePeekLast(SPBPQueue source) {
  * @return the min value in the queue
  */
 double spBPQueueMinValue(SPBPQueue source) {
+	if (source == NULL){
+		return 0;
+	}
 	//min value in the first node in queue
-	return (spListElementGetValue(spListGetFirst(source->boundedPriorityQueue)));
+	double toReturn = spListElementGetValue(spListGetFirst(source->boundedPriorityQueue));
+	return toReturn;
 }
 
 /*
@@ -172,6 +217,9 @@ double spBPQueueMinValue(SPBPQueue source) {
  * @return the max value in the queue
  */
 double spBPQueueMaxValue(SPBPQueue source) {
+	if (source == NULL){
+		return 0;
+	}
 	//max value is in the last node of the queue
 	spListGetFirst(source->boundedPriorityQueue);
 	for (int j=0 ; j < spListGetSize(source->boundedPriorityQueue); j++) {
@@ -188,6 +236,9 @@ double spBPQueueMaxValue(SPBPQueue source) {
  * @return true if the queue is empty, false otherwise
  */
 bool spBPQueueIsEmpty(SPBPQueue source) {
+	if (source == NULL){
+		return false;
+	}
 	//queue is empty <--> (source->currentSize) == 0
 	return ((spBPQueueSize(source)) == 0);
 }
@@ -199,6 +250,41 @@ bool spBPQueueIsEmpty(SPBPQueue source) {
  * @return true if the queue is full, false otherwise
  */
 bool spBPQueueIsFull(SPBPQueue source) {
+	if (source == NULL){
+		return false;
+	}
 	//queue is full <--> (source->currentSize) == (source->maxSize)
 	return ((spBPQueueSize(source)) == (source->maxSize));
+}
+
+/**
+ * switch between matching return messages from SPList
+ */
+SP_BPQUEUE_MSG switchListMessage(SP_LIST_MSG msg){
+	switch (msg){
+	case SP_LIST_SUCCESS:
+		return SP_BPQUEUE_SUCCESS;
+	case SP_LIST_NULL_ARGUMENT:
+		return SP_BPQUEUE_INVALID_ARGUMENT;
+	case SP_LIST_OUT_OF_MEMORY:
+		return SP_BPQUEUE_OUT_OF_MEMORY;
+	case SP_LIST_INVALID_CURRENT:
+		return SP_BPQUEUE_INVALID_ARGUMENT;
+	}
+	return SP_BPQUEUE_SUCCESS;
+}
+
+/**
+ * switch between matching return messages from SPListElement
+ */
+SP_BPQUEUE_MSG switchElementMessage(SP_ELEMENT_MSG msg){
+	switch (msg){
+	case SP_ELEMENT_SUCCESS:
+		return SP_BPQUEUE_SUCCESS;
+	case SP_ELEMENT_INVALID_ARGUMENT:
+		return SP_BPQUEUE_INVALID_ARGUMENT;
+	case SP_ELEMENT_OUT_OF_MEMORY:
+		return SP_BPQUEUE_OUT_OF_MEMORY;
+	}
+	return SP_BPQUEUE_SUCCESS;
 }
